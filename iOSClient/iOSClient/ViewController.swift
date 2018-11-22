cb58b0a825b66f3012f5f827149aa42b92685b42
Milad <milad@Milads-MBP.lan1>
2018-11-21 17:14:19 -0800

    initial iOSClient commit

---
+++ iOSClient/iOSClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSClient
+//
+//  Created by Milad on 11/21/18.
+//  Copyright © 2018 Milad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
