423fd798034dd3474fe9737cc24a71bd2a05263f
Milad <milad@Milads-MBP.lan1>
2018-11-27 12:10:37 -0800

    refactoring, fixed link to User.swift

---
+++ iOSClient/iOSClient/Managers/ChatManager.swift
@@ -0,0 +1,13 @@
+//
+//  ChatManager.swift
+//  iOSClient
+//
+//  Created by Milad on 11/27/18.
+//  Copyright © 2018 Milad. All rights reserved.
+//
+
+import Foundation
+
+class ChatManager {
+  
+}
