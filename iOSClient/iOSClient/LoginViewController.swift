cee267ad1075b212e83b4c7e7d66b1d560f709fa
Milad <milad@Milads-MBP.lan1>
2018-11-27 18:43:37 -0800

    UI: added LoginVC

--- iOSClient/iOSClient/LoginViewController.swift
+++ iOSClient/iOSClient/LoginViewController.swift
@@ -9,12 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+  @IBOutlet weak var usernameTextField: UITextField!
+  var username = "random \(Date().timeIntervalSinceNow)"
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    print(view.frame.size)
   }
 
-
+  @IBAction func enterChat(_ sender: Any) {
+    if usernameTextField.text != "" {
+      username = usernameTextField.text!
+    } else {
+      let randomInt = Int.random(in: 0 ..< Int(Date().timeIntervalSince1970)/100000)
+      username = "User-"+String(randomInt)
+    }
+    print(username)
+  }
+  
 }
 
---
+++ iOSClient/iOSClient/PercentLayoutConstraint.swift
@@ -0,0 +1,44 @@
+// From: http://basememara.com/percentage-based-margin-using-autolayout-storyboard/
+// May God bless LOL
+
+import UIKit
+
+/// Layout constraint to calculate size based on multiplier.
+class PercentLayoutConstraint: NSLayoutConstraint {
+  
+  @IBInspectable var marginPercent: CGFloat = 0
+  
+  var screenSize: (width: CGFloat, height: CGFloat) {
+    return (UIScreen.main.bounds.width, UIScreen.main.bounds.height)
+  }
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+    
+    guard marginPercent > 0 else { return }
+    NotificationCenter.default.addObserver(self,
+                                                     selector: #selector(layoutDidChange),
+                                                     name: UIDevice.orientationDidChangeNotification,
+                                                     object: nil)
+  }
+  
+  /**
+   Re-calculate constant based on orientation and percentage.
+   */
+  @objc func layoutDidChange() {
+    guard marginPercent > 0 else { return }
+    
+    switch firstAttribute {
+    case .top, .topMargin, .bottom, .bottomMargin:
+      constant = screenSize.height * marginPercent
+    case .leading, .leadingMargin, .trailing, .trailingMargin:
+      constant = screenSize.width * marginPercent
+    default: break
+    }
+  }
+  
+  deinit {
+    guard marginPercent > 0 else { return }
+    NotificationCenter.default.removeObserver(self)
+  }
+}
