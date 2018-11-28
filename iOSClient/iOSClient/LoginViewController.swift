b097d0b3ab3500582e1e23b2ed3e882245d6880d
Milad <milad@Milads-MBP.lan1>
2018-11-27 21:53:45 -0800

    UI: added scroll view to login

--- iOSClient/iOSClient/LoginViewController.swift
+++ iOSClient/iOSClient/LoginViewController.swift
@@ -10,22 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
   @IBOutlet weak var usernameTextField: UITextField!
+  
+  @IBOutlet weak var passwordTextField: UITextField!
+  
+  
+  
   var username = "random \(Date().timeIntervalSinceNow)"
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-    print(view.frame.size)
+
+    let tap = UITapGestureRecognizer(target: self.view, action: Selector("endEditing:"))
+    tap.cancelsTouchesInView = false
+    self.view.addGestureRecognizer(tap)
   }
 
   @IBAction func enterChat(_ sender: Any) {
     if usernameTextField.text != "" {
       username = usernameTextField.text!
+      passwordTextField.isHidden = true
     } else {
+      passwordTextField.isHidden = false
       let randomInt = Int.random(in: 0 ..< Int(Date().timeIntervalSince1970)/100000)
       username = "User-"+String(randomInt)
     }
     print(username)
+  }
+  @IBAction func createAccount(_ sender: Any) {
+    
   }
   
 }
