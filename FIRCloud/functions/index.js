0aed6137082f1b53276c2dce917b419fbbe79a52
Milad <milad@Milads-MBP.lan1>
2019-01-01 22:35:05 -0800

    firebase cloud functions

---
+++ FIRCloud/functions/index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+
+admin.initializeApp()
+
+
+exports.sendChatNotification = functions.firestore.document('channels/main/thread/{thread}').onCreate((snap, context) => {
+	var messageObject = snap.data()
+	console.log(messageObject.content)
+	const payload = {
+		notification: {
+			title: `${messageObject.senderName} @ TestChatroom/Main`,
+			body: messageObject.content,
+			sound: "default"
+		}
+	}
+	const options = {
+		priority: "high",
+		timeToLive: 60 * 60 * 24
+	}
+	return admin.messaging().sendToTopic("pushNotifications", payload, options)
+})
